1fcbdd847fafea558bd9b9f2237c76ce59585549
Rachel R <rrrachelrath@gmail.com>
2019-05-15 14:15:41 -0400

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,28 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?(".")
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
   def count_sentences
-
+    count = 0
+    array = self.split
+    array.each do |word|
+      if word.end_with?(".") == true || word.end_with?("?") == true || word.end_with?("!") == true
+        count +=1
+      end
+     # array.filter { |word| word.length > 0 }
+    #  # array.count
+    # binding.pry
+    end
+    count
   end
 end
